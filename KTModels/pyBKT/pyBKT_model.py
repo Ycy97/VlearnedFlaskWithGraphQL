931c191efe6227505db5a1675a5fb6d9393773b5
Yap Cheng Yee <chengyeeyap97@gmail.com>
2024-01-25 23:21:23 +0800

    Modified and tested BKT variatns with one student vs multiple students

--- KTModels/pyBKT/pyBKT_model.py
+++ KTModels/pyBKT/pyBKT_model.py
@@ -9,10 +9,14 @@ def BKT():
     # print(df.to_string()) 
     # print(model.params())
     test_df = pd.read_csv('VlearnedFlaskWithGraphQL/KTModels/data/test.csv')
+    test2_df = pd.read_csv('VlearnedFlaskWithGraphQL/KTModels/data/test2.csv')
     defaults = {'user_id': 'studentID', 'skill_name': 'skillComponent', 'correct': 'Correct', 'start_time': 'start',
                 'end_time': 'end', 'multigs': 'studentID'}
+    # defaults = {'user_id': 'studentID', 'skill_name': 'skillComponent', 'correct': 'Correct', 'start_time': 'start',
+    #             'end_time': 'end'}
     skill = ["addition", "subtraction"]
-    model.fit(data = test_df, defaults=defaults, skills=skill, multilearn='studentID', multigs=True)
+    #model.fit(data = test2_df, defaults=defaults, skills=skill, multilearn='studentID', multigs=True)
+    model.fit(data = test2_df, defaults=defaults, skills=skill)
     training_rmse = model.evaluate(data = test_df)
     training_auc = model.evaluate(data = test_df, metric = 'auc')
     print("Training RMSE: %f" % training_rmse)
@@ -21,8 +25,8 @@ def BKT():
     print(model.params())
 
     #predict data
-    preds = model.predict(data=test_df)
-    print(preds.head(5))
+    preds = model.predict(data=test2_df)
+    print(preds.head(10))
 
 if __name__ == "__main__":
     BKT()
